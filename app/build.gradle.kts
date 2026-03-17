b1c5f2c1654931964b8d033e5e033e1b424a90ce
Lucas Llort <lucasllort@gmail.com>
2026-03-17 00:54:15 -0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "org.example.template"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.example.template"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.guava)
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
